875ef684e98ac794b67ffed08b38578da56cd0eb
Luna1997 <luna1112@outlook.dk>
2018-12-17 11:07:23 +0100

    Found some more un-needed code

--- art.py
+++ art.py
@@ -8,6 +8,7 @@ random.seed(seed)
 s_nouns = ["Nissen","Julemanden","Slagteren","Bibliotekaren","Den undertrykte","Den mindregjordte","Den transkønnede","Den lesbiske","Vejviseren","Rotten","Musen","Anarkisten","Kommunisten"]
 p_nouns = ["malerne","politibetjentene","buschauførrene","nisserne","bigfoots","aliensne","gespensterne","pungrotterne","hundene","kattene","vennerne","gutterne","de fremmedgjordte","de mindregjordte","de homoseksuelle","de lesbiske","de transkønnede","kapitalisterne","proletariatet","arbejderne"]
 s_verbs = ["æder","slår","kysser","spiser", "sparker", "giver en kage til", "truer", "sees med", "kreere", "hacker", "konfigurer", "ser til", "klumser", "meower", "flygter fra", "automatiserer", "eksploderer"]
+#TO IMPLEMENT IN THE FUTURE
 #p_verbs = ["eat", "kick", "give", "treat", "meet with", "create", "hack", "configure", "spy on", "retard", "meow on", "flee from", "try to automate", "explode"]
 infinitives = ["for at skabe en revolution","for at ødelægge pandoras æske","for at skabe kommunisme","for at få sine ønsker opfyldt", "for at få nye venner","for at få en kæreste","for at rede verden.","for at lave en kage.", "uden nogen grund.", "fordi himlen er sort.", "for en sygdom.", "for at akkumulere mere viden."]
 title = random.choice(s_nouns), random.choice(s_verbs), random.choice(s_nouns).lower() or random.choice(p_nouns).lower(), random.choice(infinitives)
@@ -45,6 +46,5 @@ def make_img():
 		    context.move_to(random.random(),random.random())
 		    context.line_to(random.random(),random.random())
 		    context.stroke()
-		time.sleep(0.05)
 		
 make_img()
